485700c0729b00f972cf7ff254fc71f318389ed4
Sancheese95 <sasha_pavlyukovec@mail.ru>
2024-09-26 18:40:43 +0300

    Initial commit

---
+++ task_1/task_1.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+action_1 = ((-3 + a ** 2) * b - 2 ** 3)
+
+action_2 = (c - 2 * d)
+
+res = action_1 / action_2
+
+print(res)
---
+++ task_1/task_2.py
@@ -0,0 +1,6 @@
+n = int(input('Введите число минут:'))
+hours = n // 60
+minutes = n % 60
+
+print('Часов:', hours)
+print('Минут:', minutes)
